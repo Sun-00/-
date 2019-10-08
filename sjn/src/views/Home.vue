8f493da2b0aef393d99185707dc46b53ed4b008b
zzx-coder <you@example.com>
2019-10-08 19:56:50 +0800

    change

--- sjn/src/views/Home.vue
+++ sjn/src/views/Home.vue
@@ -1,13 +1,9 @@
 <template>
   <div class="home">
-    <img alt="Vue logo" src="../assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
   </div>
 </template>
 
 <script>
-// @ is an alias to /src
-import HelloWorld from '@/components/HelloWorld.vue'
 
 export default {
   name: 'home',
@@ -16,3 +12,7 @@ export default {
   }
 }
 </script>
+
+<style scoped>
+
+</style>
