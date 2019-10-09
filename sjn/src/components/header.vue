a87448dc28ae803c201066b370aca0b6a34f06cd
zzx-coder <you@example.com>
2019-10-09 09:02:12 +0800

    header

--- sjn/src/components/header.vue
+++ sjn/src/components/header.vue
@@ -5,6 +5,18 @@
           <div class="logo">
             <img src="../assets/logo.png">
           </div>
+          <div class="video">
+            <select>
+              <option value="viode">视频</option>
+              <option value="ebook">电子书</option>
+              <option value="pbook">图画书</option>
+            </select>
+            <input type="search">
+            <span class="iconfont icon-weibiaoti-"></span>
+          </div>
+          <div class="login">
+            <span class="iconfont icon-wode"></span>
+          </div>
         </div>
         <ul>
           <router-link to="/" tag="li" :class="{active:num==0}">首页</router-link>
@@ -17,6 +29,7 @@
     </div>
 </template>
 <script>
+import icon from "../font_1447810_nx33n8sgezc/iconfont.css";
 export default {
   props:['num'],
 }
@@ -41,6 +54,7 @@ export default {
   width: 100%;
   height: 54px;
   padding: 10px 0;
+  margin: 0 auto;
 }
 .head .logo{
   margin-left:200px;
@@ -58,10 +72,49 @@ export default {
   cursor: pointer;
   padding: 0 20px;
   box-sizing: border-box;
+  border: 2px dashed #fff;
 }
-.active{
+.header .active{
   border: 2px dashed #65c2ff;
   border-radius: 15px;
   box-sizing: border-box;
+}
+.header .video{
+  width: 210px;
+  height: 30px;
+  border:2px solid #00a0e9;
+  position: relative;
+  margin-left:1000px;
+  margin-top: -40px;
+}
+.header .video select{
+  border: none;
+  height: 100%;
+}
+.header .video input{
+  width: 115px;
+  height: 100%;
+  border: none;
+  outline: none;
+  position: absolute;
+  top: 0;
+  left: 70px;
+}
+.header .video span{
+  width: 30px;
+  height: 22px;
+  display: block;
+  float: right;
+  padding: 6.5px 7px;
+  box-sizing: border-box;
+}
+.header .login{
+  float: right;
+  margin-top: -27.5px;
+  margin-right: 240px;
+}
+.header .login span{
+  font-size: 24px;
+  cursor:pointer;
 }
 </style>
