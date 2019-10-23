510d0941bb32cb735c019a97549d27611f5895dd
zzx-coder <you@example.com>
2019-10-23 13:17:13 +0800

    网易

---
+++ netease/src/components/header.vue
@@ -0,0 +1,23 @@
+<template>
+  <div class="header">
+      <div class="header_top"></div>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'Header'
+}
+</script>
+
+<style scoped>
+.header{
+    width: 100%;
+    height: 220px;
+}
+.header_top{
+    width: 100%;
+    height: 36px;
+    background: rgb(51,51,51);
+}
+</style>
--- netease/src/views/Home.vue
+++ netease/src/views/Home.vue
@@ -1,14 +1,15 @@
 <template>
   <div class="home">
+    <Header></Header>
   </div>
 </template>
 
 <script>
-
-
+import Header from '../components/header'
 export default {
   name: 'home',
   components: {
+    Header
   }
 }
 </script>
