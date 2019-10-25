f83f2cad97b5f06dc9c2a99ce46564da88adccc2
Your Name <you@example.com>
2019-10-25 16:37:51 +0800

    sjn

---
+++ netease/vue.config.js
@@ -0,0 +1,22 @@
+module.exports = {
+    outputDir: 'dist',   //build输出目录
+    assetsDir: 'assets', //静态资源目录（js, css, img）
+    lintOnSave: false, //是否开启eslint
+    devServer: {
+        open: true, //是否自动弹出浏览器页面
+        host: "localhost", 
+        port: '8080', 
+        https: false,   //是否使用https协议
+        hotOnly: false, //是否开启热更新
+        proxy: null,
+		proxy: {
+			'/api': {
+				target: 'http://you.163.com/xhr/', //API服务器的地址
+				changeOrigin: true,
+				pathRewrite: {
+					'^/api': ''
+				}
+			}
+		},
+    }
+}
